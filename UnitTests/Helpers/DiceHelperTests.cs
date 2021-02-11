854e620d91facbb8a006ba021336331c5132fca7
Loi Pham <phaml5@seattleu.edu>
2021-02-10 16:17:39 -0800

    Added RollDice Invalid Roll 1 Dice 0 Should Return Zero to DiceHelperTests.cs

--- UnitTests/Helpers/DiceHelperTests.cs
+++ UnitTests/Helpers/DiceHelperTests.cs
@@ -23,12 +23,26 @@ namespace UnitTests.Helpers
         }
 
         [Test]
-        public void RollDice_Invalid_Roll_Zero_Dice_10_Should_Return_Zero()
+        public void RollDice_Invalid_Roll_0_Dice_10_Should_Return_Zero()
         {
             // Arrange
 
             // Act
             var result = DiceHelper.RollDice(0, 10);
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void RollDice_Invalid_Roll_1_Dice_0_Should_Return_Zero()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(1, 0);
 
             // Reset
 
